868c1551540dd406e32a38aae5254a36c2cf2877
Jianzhuo Y <147121033+KaCy1123@users.noreply.github.com>
2024-05-20 12:37:24 -0400

    Add files via upload

---
+++ pyFinalProject.py
@@ -0,0 +1,22 @@
+'''
+Name: Jianzhuo Ye
+Start Date: 5/20/24
+End Date: TBD
+Description
+'''
+import os.path
+from os import path
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/',methods=["GET","POST"])
+def main():
+    if request.method == 'GET':
+        return render_template('login.html');
+    else:
+        return GetLogin();
+
+def GetLogin():
+    global user, ps
+    

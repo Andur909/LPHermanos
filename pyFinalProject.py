6b6a5ced71b853bee826046b821fe67da6befa00
Jianzhuo Y <147121033+KaCy1123@users.noreply.github.com>
2024-05-22 12:43:20 -0400

    Add files via upload

--- pyFinalProject.py
+++ pyFinalProject.py
@@ -12,25 +12,52 @@ app = Flask(__name__)
 
 @app.route('/',methods=["GET","POST"])
 def main():
+    global filename;
+    filename = "logininfo.txt";
     if request.method == 'GET':
-        return render_template('login.html');
+        return render_template('LogIn.html');
     else:
         return GetLogin();
 
 def GetLogin():
-    filename = "logininfo.txt";
+    global startup
     logorsign = request.form['button']
     if logorsign == "login":
         user = request.form.get('txt_login_username');
         ps = request.form.get('txt_login_password');
+        startup = "Log_In";
     else:
         user = request.form.get('txt_signin_username');
         ps = request.form.get('txt_signin_username');
+        startup = "Sign_Up";
     userps = user + ps;
     pythfile = open(filename,"r+");
     storedinfo = pythfile.read();
     if userps in storedinfo:
-        return redirect('/settings');
+        if startup == "Sign_Up":
+            return redirect('/settings');
+        else:
+            return redirect('/order');
     else:
-        errormsg = "Username or password is incorrect"
+        errormsg = "Username or password is incorrect";
+        return render_template('login.html',start_type=startup,error_start=errormsg);
+            
+@app.route('/settings',methods=["GET","POST"])
+def settingsMain():
+    global filename;
+    filename = "addressinfo.txt";
+    info = [];
+    if request.method == "GET":
+        if startup == "Log_In":
+            pythfile=open(filename,"r+");
+            info = pythfile.readlines();
+            return render_template('Settings.html',data=info);
+        else:
+            return render_template('Settings.html');
+    else:
+        return settingsEnter();
+
+def settingsEnter():
+    
+    
     

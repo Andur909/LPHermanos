5f176bd018ee588145c25faadb1e57fd4d67aa74
Andur909 <147121202+Andur909@users.noreply.github.com>
2024-05-24 12:11:17 -0400

    Added more js
    Also remove Address L2

--- Settings.js
+++ Settings.js
@@ -13,9 +13,18 @@ function Check()
 		name = document.getElementById("lbl_current_name").innerHTML;
 		Lname = document.getElementById("lbl_current_Lname").innerHTML;
 		AddressL1 = document.getElementById("lbl_current_addressL1").innerHTML;
-		AddressL2 = document.getElementById("lbl_current_addressL2").innerHTML;
 		City = document.getElementById("lbl_current_city").innerHTML;
 		Zipcode = document.getElementById("lbl_current_zipcode").innerHTML;
+		State = document.getElementById("lbl_state").innerHTML;
 		
+		document.getElementById("txt_setting_name").value = name;
+		document.getElementById("txt_setting_Lname").value = Lname;
+		document.getElementById("txt_setting_addressL1").value = AddressL1;
+		document.getElementById("txt_setting_city").value = City;
+		document.getElementById("txt_setting_zipcode").value = Zipcode;
+		document.getElementById("cmb_state").value = State;
+		
+	} else {
+		document.getElementById("cmb_state").value = "NY";
 	}
 }
